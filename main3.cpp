1ced102f9d8e55d88c40fe6d51d25f9aac578752
Marek Doligalski <marekdoligalski@interia.eu>
2016-03-06 15:41:31 +0100

    add exec_program3:

---
+++ main3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int l;
+    int suma=0;
+    while(l!=0)
+    {
+            cin>>l;
+            suma+=l;
+    }
+    cout<<suma;
+    return 0;
+}
