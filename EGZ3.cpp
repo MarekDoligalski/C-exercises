c8d80ab54c9f213da1e9ed42457631b6a80d45cb
Marek Doligalski <marekdoligalski@interia.eu>
2016-03-06 15:30:40 +0100

    add test_3

---
+++ EGZ3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+const int n=4;
+
+
+struct Osoba
+{
+string imie;
+int ocena;
+};
+
+void wczytac (Osoba A[], Osoba B[])
+{
+for(i=0;i<n;i++){
+
+    cin >> A[i].imie;
+    cin >> A[i].ocena;
+
+    cin >> B[i].imie;
+    cin >> B[i].ocena;
+}
+}
+void zwrot (Osoba A[],Osoba B[] char M, char P)
+{
+    int roznica;
+
+
+        }
+    }
+
+    roznica =
+
+}
+
+
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
